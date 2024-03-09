5f9b88318489364e0adb99b9ce749c14f4a13d80
Psycho <139832380+hatedamon@users.noreply.github.com>
2024-03-09 14:41:38 +1000

    Add files via upload

---
+++ installer.bat
@@ -0,0 +1,14 @@
+@echo off
+rem Auto Trade Poster
+rem Author: @swedish.psycho, @hatedamon
+
+rem Install requests module
+echo Installing requests module...
+python -m pip install requests
+
+rem Install multiprocessing module
+echo Installing multiprocessing module...
+python -m pip install multiprocessing
+
+echo Installation complete.
+pause
